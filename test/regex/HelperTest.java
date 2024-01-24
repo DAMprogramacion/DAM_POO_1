4e92d92eb079908f59dd7800c24ddc264a0ab7c3
manuel <mmolmil158@g.educaand.es>
2024-01-24 15:12:46 +0100

    regex

--- test/regex/HelperTest.java
+++ test/regex/HelperTest.java
@@ -33,8 +33,57 @@ class HelperTest {
     @Test
     void validarIBAN() {
         assertNull(Helper.validarIBAN(null));
-        assertTrue(Helper.validarIBAN("es0123456789123456789"));
-        assertTrue(Helper.validarIBAN("ES123456789123456789"));
+        assertTrue(Helper.validarIBAN("es01234567890123456789"));
+        assertTrue(Helper.validarIBAN("ES01234567890123456789"));
+        assertFalse(Helper.validarIBAN("ES0123456789012345678"));
+        assertTrue(Helper.validarIBAN("ES01 23 4567 8901 23 456789"));
+        assertFalse(Helper.validarIBAN("ES1 23 4567 8901 23 456789"));
+        assertTrue(Helper.validarIBAN("ES01-23-4567-8901-23-456789"));
+        assertFalse(Helper.validarIBAN("ES1-23-4567-8901-23-456789"));
+        assertFalse(Helper.validarIBAN("ES10 23-4567-8901 23-456789"));
+        assertFalse(Helper.validarIBAN("ES10-234564890123-456789"));
+    }
 
+    @Test
+    void validarNumeroTelefono() {
+        assertNull(Helper.validarNumeroTelefono(null));
+        assertTrue(Helper.validarNumeroTelefono("666123456"));
+        assertTrue(Helper.validarNumeroTelefono("766123456"));
+        assertTrue(Helper.validarNumeroTelefono("966123456"));
+        assertFalse(Helper.validarNumeroTelefono("66612345"));
+        assertFalse(Helper.validarNumeroTelefono("866612345"));
+        assertTrue(Helper.validarNumeroTelefono("+34766123456"));
+        assertTrue(Helper.validarNumeroTelefono("+34 766123456"));
+    }
+
+    @Test
+    void validarFormatoCIF() {
+        assertNull(Helper.validarFormatoCIF(null));
+        assertTrue(Helper.validarFormatoCIF("A58818501"));
+        assertTrue(Helper.validarFormatoCIF("a58818501"));
+        assertFalse(Helper.validarFormatoCIF("A5881850a"));
+        assertTrue(Helper.validarFormatoCIF("p5881850a"));
+        assertFalse(Helper.validarFormatoCIF("p58818501"));
+        assertTrue(Helper.validarFormatoCIF("l5881850a"));
+        assertTrue(Helper.validarFormatoCIF("l58818501"));
+        assertFalse(Helper.validarFormatoCIF("l5881850a1"));
+        assertFalse(Helper.validarFormatoCIF("l588185011"));
+    }
+
+    @Test
+    void validarFechas() {
+        assertNull(Helper.validarFechas(null));
+        assertFalse(Helper.validarFechas("55/10/2000"));
+        assertFalse(Helper.validarFechas("35/10/2000"));
+        assertFalse(Helper.validarFechas("0/10/2000"));
+        assertTrue(Helper.validarFechas("15/10/2000"));
+        assertTrue(Helper.validarFechas("15/1/2000"));
+        assertTrue(Helper.validarFechas("15/01/2000"));
+        assertTrue(Helper.validarFechas("07/01/2000"));
+        assertFalse(Helper.validarFechas("15/13/2000"));
+        assertTrue(Helper.validarFechas("31/02/2000"));
+        assertTrue(Helper.validarFechas("31/02/1999"));
+        assertFalse(Helper.validarFechas("31/02/1899"));
+        assertFalse(Helper.validarFechas("31/02/3000"));
     }
 }
